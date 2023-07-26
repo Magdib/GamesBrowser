b32bccacb38f659b7c9a7777bb4334a29ab87530
Magdib <magody.ibrahem3522930@gmail.com>
2023-07-27 01:11:14 +0300

    first commit

--- lib/controller/TrailerController.dart
+++ lib/controller/TrailerController.dart
@@ -1,36 +1,36 @@
 import 'package:gamesbrowser/core/constant/ArgumentsNames.dart';
 import 'package:get/get.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+// import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 import '../core/constant/Colors.dart';
 
 class TrailerController extends GetxController {
-  late YoutubePlayerController ytController;
-  String thumbnail = Get.arguments[ArgumentsNames.thumbnail];
-  final ProgressBarColors progressBarColors = const ProgressBarColors(
-      handleColor: AppColors.white,
-      playedColor: AppColors.red,
-      backgroundColor: AppColors.white);
+  // late YoutubePlayerController ytController;
+  // String thumbnail = Get.arguments[ArgumentsNames.thumbnail];
+  // final ProgressBarColors progressBarColors = const ProgressBarColors(
+  //     handleColor: AppColors.white,
+  //     playedColor: AppColors.red,
+  //     backgroundColor: AppColors.white);
   @override
   void onInit() {
-    String videoId = YoutubePlayer.convertUrlToId(
-        Get.arguments[ArgumentsNames.youtubeLink])!;
-    ytController = YoutubePlayerController(
-      initialVideoId: videoId,
-      flags: const YoutubePlayerFlags(
-        autoPlay: true,
-        mute: false,
-        controlsVisibleAtStart: true,
-        loop: false,
-        useHybridComposition: false,
-      ),
-    );
+    // String videoId = YoutubePlayer.convertUrlToId(
+    //     Get.arguments[ArgumentsNames.youtubeLink])!;
+    // ytController = YoutubePlayerController(
+    //   initialVideoId: videoId,
+    //   flags: const YoutubePlayerFlags(
+    //     autoPlay: true,
+    //     mute: false,
+    //     controlsVisibleAtStart: true,
+    //     loop: false,
+    //     useHybridComposition: false,
+    //   ),
+    // );
     super.onInit();
   }
 
   @override
   void onClose() {
-    ytController.dispose();
+    // ytController.dispose();
     super.onClose();
   }
 }
